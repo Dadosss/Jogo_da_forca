4c941755358def98779f0d18786f02f5b4a8ba6f
Eduardo Graminho Marques <115591066+Dadosss@users.noreply.github.com>
2024-07-22 11:53:46 -0300

    Alguns incrementos.
    Adicionado alguns comentários e informações na opção SOBRE O JOGO.

--- Jogo da forca.cpp
+++ Jogo da forca.cpp
@@ -15,17 +15,19 @@ void limpaTela(){
 }
 // -----------------------
 
-
+// Função que retorna palavras aleatórias.
 string retornaPalavraAleatoria() {
+	
 	// Vetor com palavras disponíveis.
 	string palavras[9] = {"manga", "abacaxi", "morango","abacate","laranja","cadeira","paralelepipedo","otorrinolaringologista","geladeira"};
 	
-	int indiceAleatorio = rand() % 9; // Indice gerado no intervalo {0,1,2}.
+	int indiceAleatorio = rand() % 9; // Indice gerado no intervalo {0,1...,9}.
 	
 	return palavras[indiceAleatorio];
 	
 }
 
+// Retorna a palavra aleatória ou inserida pelo usuário escondida (EX: _ _ _ _ _). 
 string retornaPalavraComMascara(string palavra, int tamanhoDaPalavra){
 	
 	int cont = 0;
@@ -39,6 +41,7 @@ string retornaPalavraComMascara(string palavra, int tamanhoDaPalavra){
 	return palavraComMascara;
 }
 
+// Função que exibe os status de tamanho da palavra, tentativas e letras já arriscadas.
 void exibeStatus(string palavraComMascara, int tamanhoDaPalavra, int tentativasRestantes, string letrasJaArriscadas, string mensagem) {
 	cout << mensagem;
 	cout << "\nPalavra: " << palavraComMascara << "(Tamanho: " << tamanhoDaPalavra << ")";
@@ -80,11 +83,11 @@ int jogar(int numeroDeJogadores){
 	// Variaveis 
 	int tentativas = 0, maximoTentativas = 10; 				// Número de tentativas e erros.
 	int cont = 0;							 				// Auxiliar para laços de repetição.
-	int reiniciar;											// Escolha caso o jogador queira jogar novamente;
+	int reiniciar;											// Escolha caso o jogador queira jogar novamente.
 	char letra;												// Letra arriscada pelo jogador.
 	string letrasJaArriscadas;								// Acumula as tentativas do jogador.
 	string palavraToda;										// armazena a palavra completa que o jogador digitar.
-	string mensagem = "Bem vindo ao jogo!";					// Feedback do jogador
+	string mensagem = "Bem vindo ao jogo!";					// Feedback do jogador.
 	bool jaDigitouLetra = false, acertouLetra = false;		// Auxiliar para saber se a letra ja foi digitada.
 	
 	
@@ -95,7 +98,7 @@ int jogar(int numeroDeJogadores){
 		// Exibe o status atual do jogo.
 		exibeStatus(palavraComMascara, tamanhoDaPalavra, maximoTentativas - tentativas, letrasJaArriscadas, mensagem);
 		
-		cout << "\nDigite uma letra (Ou digite 1 para arriscar a palavra): ";	// Lê um palpite
+		cout << "\nDigite uma letra (Ou digite 1 para arriscar a palavra): ";	// Lê um palpite.
 		cin >> letra;
 		
 		if (letra == '1'){
@@ -107,7 +110,7 @@ int jogar(int numeroDeJogadores){
 				
 			}else{
 				limpaTela();
-				maximoTentativas = 0;
+				maximoTentativas = 0;	// Caso o usuário arrisque uma palavra inteira e erre, zera suas tentativas e ele perde automaticamente.
 				cout << "\nVocê errou a palavra\n\n";
 			}
 		}
@@ -147,10 +150,10 @@ int jogar(int numeroDeJogadores){
 				mensagem = "Você acertou uma letra!";
 			}
 		
-			tentativas ++; // Aumenta uma tentativa realizada	
+			tentativas ++; // Aumenta uma tentativa realizada.	
 		}
 			
-			// Reinicia auxiliares
+			// Reinicia as variáveis auxiliares.
 			jaDigitouLetra = false;
 			acertouLetra = false;
 			
@@ -187,7 +190,7 @@ void menuInicial(){
 		cout << "JOGO DA FORCA\n";
 		cout << "1 - Jogar sozinho\n";
 		cout << "2 - Jogar em dupla\n";
-		cout << "3 - Sobre\n";
+		cout << "3 - Sobre o jogo\n";
 		cout << "4 - Sair\n";
 		cout << "Escolha uma opçao e tecle ENTER: ";
 		cin >> opcao; // Armazena a opção escolhida pelo jogador.
@@ -217,7 +220,8 @@ void menuInicial(){
 			case 3:
 				limpaTela();
 				int voltar;
-				cout << "\nINFORMAÇÕES DO JOGO\n" <<"\nJogo desenvolvido em 07/07/2024 a título de estudo.\n";
+				cout << "\nINFORMAÇÕES DO JOGO\n" <<"\nJogo desenvolvido em 07/07/2024 a título de estudo.\n\n";
+				cout << "Desenvolvedor: Eduardo Graminho Marques.\nContato (41) 9 91960198\n";
 				cout << "\n1 - Voltar" << "\n2 - sair\n";
 				cin >> voltar;
 					
@@ -240,9 +244,6 @@ void menuInicial(){
 }
 // FIM da função que mostra o Menu Inicial para o jogador.
 
-void reiniciaJogo(){
-	menuInicial();
-}
 
 int main() {
 	setlocale(LC_ALL,""); // Permite letras com  acentos serem exibidas no terminal.
